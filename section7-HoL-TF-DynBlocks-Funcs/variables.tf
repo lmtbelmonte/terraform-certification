c0e8905a206bd09c78cd57bc4a1c806bac4364c1
lmtbelmonte <lmtbelmonte@gmail.com>
2021-04-05 19:16:55 +0200

    Modidficacion tags y variables.tf

--- section7-HoL-TF-DynBlocks-Funcs/variables.tf
+++ section7-HoL-TF-DynBlocks-Funcs/variables.tf
@@ -21,6 +21,30 @@ variable "rules" {
       cidr_blocks = ["6.7.8.9/32"]
     }
   ]
+}
 
+# sustituimos los valores por variables
+variable "azs" {
+  type    = list(string)
+  default = ["us-east-1a"]
+}
 
+variable "vpc_name" {
+  type    = string
+  default = "lmt-vpc"
+}
+
+variable "public_subnets" {
+  type    = list(string)
+  default = ["10.0.1.0/24"]
+}
+
+variable "cidr_vpc" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t3.micro"
 }
